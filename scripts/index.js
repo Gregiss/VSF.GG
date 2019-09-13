eb52cde775f3368819e82a7d0d3cccdf2b8eb0e4
Alexandre Silva <xande1231231@hotmail.com>
2019-09-13 12:59:05 -0300

    WIP

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,13 @@ var app = new Vue({
         linkAtivo: { "name": "Home", "ativo": true},
         domain: "VSF.GG",
         user: {"nickname" : null},
-        logged: false
+        logged: false,
+        api: "RGAPI-4047f1e0-4555-42e9-9423-465b734b71ee"
     },
     mounted() {
         document.title = this.domain
+        //this.post("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/vsf%20alek%20vs?api_key=" + this.api)
+        
     },
     methods: {
         acessLink(link) {
@@ -24,10 +27,20 @@ var app = new Vue({
             this.linkAtivo = this.links[index]
             document.title = "VSF.GG |" + " " + link.name
         },
+        post(url){
+            axios.get(url)
+            .then((res) => {
+                console.log("RESPONSE RECEIVED: ", res);
+            })
+            .catch((err) => {
+            console.log("AXIOS ERROR: ", err);
+            })
+        },
         changeNickName(){
             if(this.user.nickname == '' || this.user.nickname == null)
             {
                  this.logged = false
+                 this.post("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/kaway404?api_key=" + this.api)
             }
             else{
                 this.logged = true
